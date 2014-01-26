763802f04cafe1f560266673ab0d3dd66cf8ea97
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-26 12:52:03 -0500

    Used timeouts for moving the ball and removing tiles to display collisions.

--- examples/platformCollisions.js
+++ examples/platformCollisions.js
@@ -17,16 +17,15 @@ var platforms = [
     [5, 5, 13],
     [10, 5, 13],
     [15, 5, 13],
-    [14, 10, 13],
+    [9, 10, 13],
+    [14, 5, 8]
 ];
 var radius = 10,
     ball = {
-        x: 190,
-        y: 50,
         box: {
             x: 190,
             y: 50,
-            left: -radius,
+            left: radius,
             right: radius,
             top: radius,
             bottom: radius
@@ -62,32 +61,41 @@ setInterval(function () {
     drawPlatforms();
     // Object copy
     var copy = {
-        x: ball.x + deltaX,
-        y: ball.y + deltaY
+        x: ball.box.x + deltaX,
+        y: ball.box.y + deltaY
     };
     // Collision check
-    var allowedPosition = canvas.collisionMatrix.checkVertical(ball.box, copy);
-    ball.y = allowedPosition.y;
-    // ball.x = allowedPosition.x;
-     
-    
+    var allowedX = canvas.collisionMatrix.checkHorizontal(ball.box, copy);
+    ball.box.x = copy.x = allowedX.x;
+    allowedY = canvas.collisionMatrix.checkVertical(ball.box, copy);
+    ball.box.y = allowedY.y;
+
     // Drawing
     context.fillStyle = '#aaa';
-    context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
+    context.arc(ball.box.x, ball.box.y, ball.radius, 0, 2 * Math.PI);
     context.fill();   
 }, 33);
 
 // Remove tiles
-removeTiles = [[5, 9], [10, 9], [14, 10], [14, 11], [14, 12], [14, 13]];
-setInterval(function () {
+removeTiles = [[5, 9], [9, 10], [9, 11], [9, 12], [10, 12], [14, 8], [14, 7], [14, 6], [0, 0], [10, 6]];
+var remove = setInterval(function () {
     var tile;
     if ((tile = removeTiles.shift())) {
         canvas.collisionMatrix.setValue(tile[0], tile[1], false);
+        return;
     }
+    clearInterval(remove);
 }, 1000);
 setTimeout(function () {
     deltaX = 7;
-}, 3500);
+}, 3000);
 setTimeout(function () {
     deltaX = 0;
-}, 6500);
+}, 5000);
+setTimeout(function () {
+    deltaX = -7;
+}, 6000);
+setTimeout(function () {
+    deltaX = 0;
+    deltaY = -7;
+}, 9000);
